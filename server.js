8346735cb00a760ef45b12ae62fdfd73a9fd91b7
igorcybersec1 <igorsoaresmain@gmail.com>
2026-02-20 12:11:14 -0300

    add https protocol

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const app = require('./app');
 require('dotenv').config();
+const https = require('https');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 8800;
+const options = {
 
-app.listen(PORT, '0.0.0.0', () => {
+    key: fs.readFileSync(process.env.KEY),
+    cert: fs.readFileSync(process.env.CERT)
+}
+
+https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
 
     console.log(`Servidor rodando na porta ${PORT}`);
-})
-
+});
